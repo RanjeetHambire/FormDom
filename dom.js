4f94a7a8d2a368b59609889b2ddfcad113b1d141
Ranjeet Hambire <ranjeethambire96@gmail.com>
2023-08-31 23:21:58 +0530

    third commit

--- dom.js
+++ dom.js
@@ -1,7 +1,8 @@
 // console.log("Hii Ranjeet");
 
 const header = document.getElementById('main-header');
-const item = document.getElementsByClassName('list-group-item')
+const item = document.getElementsByClassName('list-group-item');
+const li = document.getElementsByTagName('li');
 
 header.style.border='solid 3px black';
 
@@ -10,4 +11,8 @@ item[2].style.backgroundColor = 'Green';
 
 for(var i =0; i<item.length;i++){
     item[i].style.fontWeight = 'bold';
+}
+
+for(var i=0; i<li.length;i++){
+    li[i].style.color='green';
 }
