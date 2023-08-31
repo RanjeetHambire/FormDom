865f2b5efc361ccfca9ff0de47d6f68d1538fba7
Ranjeet Hambire <ranjeethambire96@gmail.com>
2023-08-31 22:57:19 +0530

    bold items

--- dom.js
+++ dom.js
@@ -4,5 +4,10 @@ const header = document.getElementById('main-header');
 const item = document.getElementsByClassName('list-group-item')
 
 header.style.border='solid 3px black';
-item[0].style.fontWeight = 'bold';
+
 item[0].style.color = 'green';
+item[2].style.backgroundColor = 'Green';
+
+for(var i =0; i<item.length;i++){
+    item[i].style.fontWeight = 'bold';
+}
