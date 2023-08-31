3a59f9d192f06d5b3c5a6c43628324135a7bb524
Ranjeet Hambire <ranjeethambire96@gmail.com>
2023-08-31 22:14:50 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,8 @@
+// console.log("Hii Ranjeet");
+
+const header = document.getElementById('main-header');
+const item = document.getElementsByClassName('list-group-item')
+
+header.style.border='solid 3px black';
+item[0].style.fontWeight = 'bold';
+item[0].style.color = 'green';
